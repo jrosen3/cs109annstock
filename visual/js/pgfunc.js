dd6188b8812eaf267dbb16a900d94a5f6c947fdb
Kevin Sun <kevnsn@gmail.com>
2013-12-08 21:30:04 -0500

    Added visualization features

--- visual/js/pgfunc.js
+++ visual/js/pgfunc.js
@@ -5,31 +5,59 @@
 $(function() {
     console.log( "The page has loaded, pagefunctions.js running" );
 	$('#introModal').modal()
-	inputdata = [10,12,6,8,15];
-	hiddendata = [10,12,6,8,15];
- 
-    selectDIV = d3.select("#visualization");
+	inputdata = [10,12,6,8,15,18,19,32,54,87,45,50,23,55,34];
+	hiddendata = [10,12,6,8,67,3,6];
+ 	outputdata = [10];
+	
+    mainSVG = d3.select("#mainvis").append('svg');
+	
+	var xmargin=70;
+	var xspace=80;
+	var ymargin=50;
+	var yspace=40;
 		 
-	inputcircles=selectDIV.selectAll("circle");
+	inputcircles=mainSVG.selectAll("circle");
 	
 	inputcircles
 	 .data(inputdata)
+	 .enter().append("circle")
+	 .attr("cy", function(d, i) {return i * yspace + ymargin;})
+	 .attr("cx", xmargin)
+	 .attr("stroke", "#bbbbbb")
+	 .attr("fill","#cccccc")
+	 .attr("r", 15);
+ 
+	 var nodetext = mainSVG.selectAll("text")
+	 .data(inputdata)
 	 .enter()
-	 .append("circle");
+	 .append("text")
 	 
-	selectDIV.selectAll("circle")
-	 .attr("cy", function(d, i) {return i * 80 + 50;})
-	 .attr("cx", 50)
-	 .attr("stroke", "blue")
-	 .attr("r", 20);
+	 var nodelabels = nodetext.attr("x", function(d) { return xmargin })
+                 .attr("y", function(d, i) {return i * yspace + ymargin+5;})
+                .text( function (d) { return d+""; })
+                 .attr("font-family", "sans-serif")
+                 .attr("font-size", "12px")
+                 .attr("fill", "#333")
+				 .attr("text-anchor","middle");
  	
-	 layercircles=selectDIV
+	 layercircles=mainSVG.selectAll("derp1")
     	.data(hiddendata)
   		.enter().append("circle")
-	 .attr("cy", function(d, i) {return i * 80 + 90;})
-	 .attr("cx", 80)
-	 .attr("stroke", "blue")
-	 .attr("r", 20);
+	 .attr("cy", function(d, i) {return i * yspace + ymargin+.5*yspace;})
+	 .attr("cx", xmargin+xspace)
+	 .attr("stroke", "#bbbbbb")
+	 .attr("fill","#cccccc")
+	 .attr("r", 15);
+	 
+	 outputcircles=mainSVG.selectAll("derp2")
+    	.data(outputdata)
+  		.enter().append("circle")
+	 .attr("cy", function(d, i) {return i * yspace + ymargin + 1*yspace;})
+	 .attr("cx", xmargin+2*xspace)
+	 .attr("stroke", "#bbbbbb")
+	 .attr("fill","#cccccc")
+	 .attr("r", 15);
  	
+	
  
 });
